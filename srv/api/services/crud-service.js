296ffe002d3567faa59d88eb3c9e10b36b15abeb
rojas <rojas.devnet@gmail.com>
2025-10-20 20:13:45 -0600

    add one working, addMany no

--- srv/api/services/crud-service.js
+++ srv/api/services/crud-service.js
@@ -8,7 +8,6 @@ export const crudErrores = async (params, body) => {
 
   const { queryType, LoggedUser, dbServer } = params;
 
-  // let result;
   try {
     if (!functionsDic[queryType]) {
       const err = new Error(`Unsupported queryType: ${queryType}`);
@@ -21,73 +20,7 @@ export const crudErrores = async (params, body) => {
       throw err;
     }
 
-    bitacora = await functionsDic[queryType](params, bitacora);
-    // switch (queryType) {
-    //   case 'getAll':
-    //     //OBTENER LA DATA CON EL CONTROLADOR PARA ERRORES
-    //     result = await zterrorlogService.GetAllErrors();
-    //     result = JSON.parse(result); //PARSEAR RESULTADO A JSON
-
-    //     //ASIGNAR LOS CAMPOS Y VALORES A BITACORA
-    //     bitacora.data.push(result.data);
-    //     bitacora.countData = result.results;
-    //     bitacora.success = true;
-    //     bitacora.status = 200;
-    //     bitacora.loggedUser = LoggedUser;
-    //     bitacora.finalRes = true;
-    //     bitacora.dbServer = dbServer;
-    //     break;
-
-    //   case 'getOne':
-    //     if (!id) throw new Error('Missing ID parameter');
-    //     bitacora = await zterrorlogService.GetOneError(id);
-    //     break;
-
-    //   case 'add':
-    //     if (whatTypeVarIs(body.data) === 'isObject') {
-    //       const error = body.data;
-    //       const result = await zterrorlogService.InsertOneError(error);
-    //       if (result) {
-    //         //ASIGNAR LOS CAMPOS Y VALORES A BITACORA
-    //         bitacora.data.push(error);
-    //         bitacora.countData = 1;
-    //         bitacora.success = true;
-    //         bitacora.status = 201;
-    //         bitacora.loggedUser = LoggedUser;
-    //         bitacora.finalRes = true;
-    //         bitacora.dbServer = dbServer;
-    //       }
-    //       return;
-    //     }
-    //     const errors = body.data;
-    //     const promesas = errors.map((error) =>
-    //       zterrorlogService.InsertOneError(error)
-    //     );
-
-    //     try {
-    //       const resultados = await Promise.all(promesas);
-    //       resultados.forEach((resultado) => bitacora.data.push(resultado));
-    //       bitacora.countData = resultados.length;
-    //       bitacora.success = true;
-    //       bitacora.status = 201;
-    //       bitacora.loggedUser = LoggedUser;
-    //       bitacora.finalRes = true;
-    //       bitacora.dbServer = dbServer;
-    //     } catch (errorBita) {
-    //       bitacora.countData =errors.length;
-    //       bitacora.success = false;
-    //       bitacora.status = 589;
-    //       bitacora.loggedUser = LoggedUser;
-    //       bitacora.finalRes = true;
-    //       bitacora.dbServer = dbServer;
-    //       return FAIL(bitacora);
-    //     }
-
-    //     break;
-
-    //   default:
-    //     throw new Error(`Unsupported queryType: ${queryType}`);
-    // }
+    bitacora = await functionsDic[queryType](params, bitacora, body);
 
     return OK(bitacora);
   } catch (errorBita) {
@@ -97,10 +30,6 @@ export const crudErrores = async (params, body) => {
     bitacora.messageUSR = errorBita.message; // process: '';
     bitacora.messageDEV = errorBita.message;
     bitacora.countData = 0;
-    // bitacora.// countDataReq: 0;
-    // bitacora.// countDataRes: 0;
-    // bitacora.// countMsgUSR: 0;
-    // bitacora.// countMsgDEV: 0;
     bitacora.dbServer = dbServer || 'not provided';
     // bitacora.data: [];
     bitacora.loggedUser = LoggedUser || 'not provided'; // server: '';
--- srv/middlewares/queryDic.js
+++ srv/middlewares/queryDic.js
@@ -1,11 +1,12 @@
 import zterrorlogService from '../api/services/zterrorlog-service.js';
+import { whatTypeVarIs } from '../helpers/variables.js';
 
 const getAllFunction = async (params, bitacora) => {
   const { LoggedUser, dbServer } = params;
 
   try {
     let result = await zterrorlogService.GetAllErrors();
-    // result = JSON.parse(result); //PARSEAR RESULTADO A JSON
+    result = JSON.parse(result); //PARSEAR RESULTADO A JSON
 
     //ASIGNAR LOS CAMPOS Y VALORES A BITACORA
     bitacora.data.push(result.data);
@@ -66,7 +67,64 @@ const getOneFunction = async (params, bitacora) => {
     return bitacora;
   }
 };
+
+const addFunciton = async (params, bitacora, body) => {
+  const { LoggedUser, dbServer, id } = params;
+  const data = body.data;
+
+  console.log(`datos: ${data}`);
+
+  try {
+    if (!data) {
+      throw new Error('Missing data');
+    }
+    let result;
+    if (whatTypeVarIs(body.data) === 'isArray') {
+      data.map(async (error) => {
+        result = await zterrorlogService.InsertOneError(error);
+        result = JSON.parse(result);
+        //ASIGNAR LOS CAMPOS Y VALORES A BITACORA
+        bitacora.data.push(result.data);
+        bitacora.countData = result.results;
+        bitacora.success = true;
+        bitacora.status = 200;
+        bitacora.loggedUser = LoggedUser;
+        bitacora.finalRes = true;
+        bitacora.dbServer = dbServer;
+        bitacora.messageUSR = 'Errores insertados correctamente';
+        bitacora.messageDEV = 'Errores insertados correctamente';
+        return bitacora;
+      });
+    }
+    const error = body.data;
+    result = await zterrorlogService.InsertOneError(error);
+    result = JSON.parse(result);
+    if (result) {
+      //ASIGNAR LOS CAMPOS Y VALORES A BITACORA
+      bitacora.data.push(error);
+      bitacora.countData = 1;
+      bitacora.success = true;
+      bitacora.status = 201;
+      bitacora.loggedUser = LoggedUser;
+      bitacora.finalRes = true;
+      bitacora.dbServer = dbServer;
+      return bitacora;
+    }
+  } catch (errorBita) {
+    bitacora.countData = 0;
+    bitacora.success = false;
+    bitacora.status = 525;
+    bitacora.loggedUser = LoggedUser;
+    bitacora.finalRes = true;
+    bitacora.dbServer = dbServer;
+    bitacora.messageDEV = errorBita.message;
+    bitacora.messageUSR = 'Error al insertar errores';
+    return bitacora;
+  }
+};
+
 export const functionsDic = {
   getAll: (params, bitacora) => getAllFunction(params, bitacora),
   getOne: (params, bitacora) => getOneFunction(params, bitacora),
+  add: (params, bitacora, body) => addFunciton(params, bitacora, body),
 };
